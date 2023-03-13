f3747599aac6f4066092505f0d0a152af590f9ab
hsem4717 <hsem4717@gmail.com>
2023-03-13 11:16:06 +0900

    3/13

--- javaclass2/src/ch09/car.java
+++ javaclass2/src/ch09/car.java
@@ -1,4 +1,38 @@
 package ch09;
 
 public class car {
+
+    private int speed;
+    private String company;
+    private String model;
+    private String color;
+
+    public car(String company, String model, String color){
+        this.company = company;
+        this.model = model;
+        this.color = color;
+    }
+
+    public void speedUp(){
+        System.out.println("가속");
+        speed += 10;
+    }
+    public void stopCar(){
+        while(speed !=0){
+            speedDown();
+        }
+    }
+
+    private void speedDown() {
+        if(speed <= 0){
+            System.out.println("정지");
+            return;
+        }
+        speed -=10;
+        System.out.println("감속");
+    }
+
+    public void showInfo(){
+        System.out.println("제조사: "+company+" 차종: "+model+" 색깔: "+color);
+    }
 }
